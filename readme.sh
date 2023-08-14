0aa6c977a8aed9e455c0ecf00815f3d437dc7e3f
RichardMiruka <richardonsare15@gmail.com>
2023-08-14 07:16:38 -0700

    README.md bash scripts

---
+++ readme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                                               
+# Ask user for project name and description
+echo "Enter project name: "
+read project_name
+
+echo "Enter project description: "
+read project_description
+
+# Create README.md file with project name and description
+echo "# $project_name" > README.md
+echo "" >> README.md
+echo "$project_description" >> README.md
+
+echo "README.md created successfully!"
